db56aa88c632e9c7c70e85cd0da9c0e428a0b23f
Guezin <leandroguezinjunior@gmail.com>
2021-12-13 16:58:52 -0300

    feat: create lambda get latest launch

---
+++ src/launches/latest_launch/handler.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+def handler(event, context):
+    response = requests.get('https://api.spacexdata.com/v4/launches/latest')
+
+    if response.status_code is not 200:
+      raise Exception(f'Error - Status API SpaceX: {response.status_code}')
+
+    latest_launch_data = response.json()
+
+    latest_launch = {
+      'name': latest_launch_data['name'],
+      'date_utc': latest_launch_data['date_utc'],
+      'date_local': latest_launch_data['date_local'],
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(latest_launch, ensure_ascii=False).encode('utf8'),
+    }
