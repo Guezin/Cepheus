e7097bb8be96f33bca6ba3d1acdff5cda566a2a3
Guezin <leandroguezinjunior@gmail.com>
2021-12-13 17:25:39 -0300

    feat: create lambda get upcoming launches

---
+++ src/launches/upcoming/handler.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+def handler(event, context):
+    response = requests.get('https://api.spacexdata.com/v4/launches/upcoming')
+
+    if response.status_code != 200:
+      raise Exception(f'Error - Status API SpaceX: {response.status_code}')
+
+    upcoming_launches_data = response.json()
+
+    upcoming_launches = [
+      { 
+        'name': launch['name'], 
+        'date_utc': launch['date_utc'], 
+        'date_local': launch['date_local']
+      }
+      for launch in upcoming_launches_data
+    ]
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(upcoming_launches, ensure_ascii=False).encode('utf8'),
+    }
